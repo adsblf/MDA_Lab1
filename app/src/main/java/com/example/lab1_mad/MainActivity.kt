970d5db320e9e58a6d2ab114d8c248ceff8de6ed
Aleksey Berezhnov <v4tary@yandex.ru>
2023-04-06 19:42:21 +0600

    Add Blank Fragment

--- app/src/main/java/com/example/lab1_mad/MainActivity.kt
+++ app/src/main/java/com/example/lab1_mad/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragment = BlankFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, fragment)
+            .commit()
     }
 }
